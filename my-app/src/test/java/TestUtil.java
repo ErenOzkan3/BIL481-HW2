042df2827fbc9520175ca874133d998361798dd9
Duayen Coder <sasiifanii@gmail.com>
2024-03-26 14:21:01 +0300

    Added more tests to TestUitl.java

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -31,4 +31,19 @@ public class TestUtil {
     assertEquals(false, c.compute(1, 2, -2, -1));
     assertEquals(false, c.compute(1, 2, -2, -1, 17, 84, 369, 440));
   }
+
+  @Test
+  public void testForValidParameters(){
+    assertEquals(true, c.compute(1, 2, -4));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testForThrowedException(){
+    c.compute(0,1,2);
+  }
+
+  @Test
+  public void testForComputation(){
+    assertEquals(false, c.compute(3, 5, 9));
+  }
 }
